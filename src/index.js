6a7018af198ec46b72de9866afc225e129a07fc1
shiva <shivangi31012004@gmail.com>
2025-05-02 13:53:04 +0530

    day01

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
-import PDFDocument from 'pdfkit';
-import fs from 'fs';
+//import PDFDocument from 'pdfkit';
+//import fs from 'fs';
 
 const app = express();
 const server = createServer(app);
@@ -14,24 +14,24 @@ const io = new Server(server); // Attach Socket.IO to the HTTP server
 
 // Install with: npm install pdfkit
 
-function createBrochure(res, brochureData) {
-  const doc = new PDFDocument();
+// function createBrochure(res, brochureData) {
+//   const doc = new PDFDocument();
   
-  // Set response headers for download
-  res.setHeader('Content-Disposition', 'attachment; filename=brochure.pdf');
-  res.setHeader('Content-Type', 'application/pdf');
+//   // Set response headers for download
+//   res.setHeader('Content-Disposition', 'attachment; filename=brochure.pdf');
+//   res.setHeader('Content-Type', 'application/pdf');
 
-  doc.pipe(res); // Stream PDF to client
+//   doc.pipe(res); // Stream PDF to client
 
-  doc.fontSize(26).text('Welcome to Our App!', { align: 'center' });
-  doc.moveDown();
-  doc.fontSize(14).text(brochureData.description || 'This is a sample brochure.');
+//   doc.fontSize(26).text('Welcome to Our App!', { align: 'center' });
+//   doc.moveDown();
+//   doc.fontSize(14).text(brochureData.description || 'This is a sample brochure.');
   
-  // Add image (optional)
-  // doc.image('path/to/image.png', {width: 150});
+//   // Add image (optional)
+//   // doc.image('path/to/image.png', {width: 150});
 
-  doc.end();
-}
+//   doc.end();
+// }
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -47,11 +47,11 @@ io.on('connection', (socket) => {
     });
   });
 
-  app.post('/generate-brochure', (req, res) => {
-    const brochureData = req.body;
-    createBrochure(res, brochureData); // Function from above
-    res.send('Brochure generated!'); // Send a response to the client
-  });
+  // app.post('/generate-brochure', (req, res) => {
+  //   const brochureData = req.body;
+  //   createBrochure(res, brochureData); // Function from above
+  //   res.send('Brochure generated!'); // Send a response to the client
+  // });
 // Start the server
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
