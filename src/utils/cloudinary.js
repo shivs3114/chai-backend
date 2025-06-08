a585bf57ec13ae7b139ad5e526aaf0c3d31f3631
shiva <shivangi31012004@gmail.com>
2025-06-09 00:20:03 +0530

    ;ecture 10 cont

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs'; 
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,});
+
+ const uploadOnCloudinary = async (localfilePath) => {
+    try {
+        if(!localfilePath) return null;
+        const result = await cloudinary.uploader.upload(localfilePath, {
+            resource_type: 'auto',
+        });
+        //return result;
+        console.log('File uploaded successfully:', result.url);
+        return result;
+    } catch (error) {
+        fs.unlinkSync(localfilePath); // Delete the file if upload fails
+        console.error('Error uploading to Cloudinary:', error);
+        throw null;
+    }}
+
+    export {uploadOnCloudinary}
