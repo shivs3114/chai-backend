e2a504dffc79236c145af338e6aa11d4311ab081
shiva <shivangi31012004@gmail.com>
2025-07-01 23:53:20 +0530

    register controller bugs fixed lecture-15

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -6,37 +6,42 @@ import { ApiResponse } from '../utils/ApiResponse.js';
 
 const registerUser=asyncHandler(async (req, res) => 
    { //middleware to handle file uploads
-   const {username,password,email,fullname} = req.body;
-   if([fullname,email,username,password].some((field)=>
+   const {username,password,email,fullName} = req.body;
+   console.log(req.body);
+   
+   if([username,password,email,fullName].some((field)=>
       field?.trim()==="")){
        throw new ApiError(400,"All fields are required"); 
    }
-   User.findOne({
+   const existedUser=await User.findOne({
       $or:[{username},{email}]}
-      ).then((user)=>{throw new ApiError(
-
-         "Username or email already exists",409)})
+      )
+     if(existedUser){
+      throw new ApiError("Username or email already exists",400);} 
 
    const avatarLocalPath=req.files?.avatar[0]?.path;
-   const coverImagePath=req.files?.coverimage[0]?.path;
+   //const coverImagePath=req.files?.coverimage[0]?.path;
    if(!avatarLocalPath)
    {throw new ApiError("Avatar is required",400);}
    const avatar=await uploadOnCloudinary(avatarLocalPath)
+   let coverImagePath;
+   if(req.files &&Array.isArray(req.files.coverimage) && req.files.coverimage.length > 0) {
+      coverImagePath = req.files.coverimage[0].path;
+   }
 
    const coverimage=await uploadOnCloudinary(coverImagePath);
    if(!avatar){
       throw new ApiError("Avatar upload failed",400);
    }
 
-   const user=await User.create({fullname,
-      avatar:avatar.url
-  ,
+   const user=await User.create({fullName,
+      avatar:avatar.url,
 coverimage:coverimage?.url||"",
 email,
 password,
 username:username.toLowerCase(),   })
 
-  const createdUser= await User.findById(user._id).select("-password -refreshToken"); //string me soace dekar likhna hota kya nahi chahiye
+  const createdUser= await User.findById(user._id).select("-password -refreshToken"); //string me space dekar likhna hota kya nahi chahiye
   if(!createdUser){
       throw new ApiError("User creation failed",500);
   }
